54b32e8c18df79e3e455fd98b8ed488fa0d03614
QiMeng <616684889@qq.com>
2014-11-01 21:36:55 +0800

    Forecast
    微预报

---
+++ Forecast/Forecast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Forecast
+//
+//  Created by 永生刘 on 14/11/1.
+//  Copyright (c) 2014年 QiMENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Forecast/Forecast/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Forecast
+//
+//  Created by 永生刘 on 14/11/1.
+//  Copyright (c) 2014年 QiMENG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
